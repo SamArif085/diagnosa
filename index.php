8e03886263b5f0b32f98fbd00813359a047efa0c
YASA1102 <94970015+YASA1102@users.noreply.github.com>
2022-07-09 13:41:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
     </header>
 
     <main>
-        <div class="container ">
+        <div class="content">
             <?php
             if (isset($_GET['page'])) {
                 $page = $_GET['page'];
@@ -83,18 +83,18 @@
                 include "support/home.php";
             }
             ?>
+            <!-- FOOTER -->
+            <footer class="">
+                <p class="float-end"><a href="#">Back to top</a></p>
+                <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+            </footer>
         </div>
-
-
-        <!-- FOOTER -->
-        <footer class="container">
-            <p class="float-end"><a href="#">Back to top</a></p>
-            <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-        </footer>
     </main>
 
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="../assets/dist/js/bootstrap.bundle.min.js"></script> -->
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <script src="cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
